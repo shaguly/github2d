0f7f9f0638bebbedecef9485929190805617c55a
shaguly <dasoguzwelayat95@gmail.com>
2022-03-03 18:58:25 +0500

    ish tamom

--- javascript/index.js
+++ javascript/index.js
@@ -1,22 +1,28 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let a = 5;
-let b = 10;
-if (a > b) {
-    console.log('a katta');
-}
-else {
-    console.log('b katta');
-}
+// let a: number = 5
+// let b: number = 10
+// if(a>b){
+// console.log('a katta');
+// }else{
+//     console.log('b katta');
+// }
+// export{}
+// let km: number= 1.5;
+// let ft: number=40;
+// let natija = 0;
+// let natija2 = 0;
+// natija = km * 1000;
+// natija2 = ft * 0.305
+// if(natija > natija2){
+//     console.log('natija katta')
+// }else{
+//     console.log('natija2 katta')
+// }
 let km = 1.5;
-let ft = 40;
-let natija = 0;
-let natija2 = 0;
-natija = km * 1000;
-natija2 = ft * 0.305;
-if (natija > natija2) {
-    console.log('natija katta');
+let coat = 2;
+if (km * 3.6 > coat) {
+    console.log('ms katta');
 }
 else {
-    console.log('natija2 katta');
+    console.log('coat katta');
 }
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,24 +1,35 @@
-let a: number = 5
-let b: number = 10
-if(a>b){
-console.log('a katta');
+// let a: number = 5
+// let b: number = 10
+// if(a>b){
+// console.log('a katta');
+// }else{
+//     console.log('b katta');
+// }
+
+// export{}
+
+// let km: number= 1.5;
+// let ft: number=40;
+// let natija = 0;
+// let natija2 = 0;
+
+// natija = km * 1000;
+// natija2 = ft * 0.305
+
+// if(natija > natija2){
+//     console.log('natija katta')
+// }else{
+//     console.log('natija2 katta')
+
+// }
+
+let km: number = 1.5;
+let coat: number= 2;
+if(km * 3.6 > coat){
+console.log('ms katta')
 }else{
-    console.log('b katta');
+console.log('coat katta')
+
 }
 
-export{}
 
-let km: number= 1.5;
-let ft: number=40;
-let natija = 0;
-let natija2 = 0;
-
-natija = km * 1000;
-natija2 = ft * 0.305
-
-if(natija > natija2){
-    console.log('natija katta')
-}else{
-    console.log('natija2 katta')
-
-}
