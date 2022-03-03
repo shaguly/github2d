a781e9d38d08d9ed4a436372320cc3f57207058e
shaguly <dasoguzwelayat95@gmail.com>
2022-03-03 18:52:17 +0500

    ts masala

---
+++ javascript/index.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let a = 5;
+let b = 10;
+if (a > b) {
+    console.log('a katta');
+}
+else {
+    console.log('b katta');
+}
+let km = 1.5;
+let ft = 40;
+let natija = 0;
+let natija2 = 0;
+natija = km * 1000;
+natija2 = ft * 0.305;
+if (natija > natija2) {
+    console.log('natija katta');
+}
+else {
+    console.log('natija2 katta');
+}
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,2 +1,24 @@
+let a: number = 5
+let b: number = 10
+if(a>b){
+console.log('a katta');
+}else{
+    console.log('b katta');
+}
 
-console.log("salom dunyo")
+export{}
+
+let km: number= 1.5;
+let ft: number=40;
+let natija = 0;
+let natija2 = 0;
+
+natija = km * 1000;
+natija2 = ft * 0.305
+
+if(natija > natija2){
+    console.log('natija katta')
+}else{
+    console.log('natija2 katta')
+
+}
